82feb4c569ad272a27f4a5cc31a625bd8b851e4b
kdykrg <Dmitriy_Kravets@epam.com>
2017-11-09 15:10:31 +0600

    disk free space added script

---
+++ Disk_free_space.ps1
@@ -0,0 +1,14 @@
+﻿Get-WmiObject -Class Win32_LogicalDisk | ForEach-Object {
+$freespace_percents = (($_.freespace/$_.size)*100)
+$freespace_percents}
+
+
+foreach ( $item in (Get-WmiObject -Class Win32_LogicalDisk))
+ {
+    if (($item.freeSpace/$item.Size) -le 0.7)
+     
+        {
+    " Please check disk " + $item.DeviceID +"!"+ " Available free disk space is " +  [math]::Round((($item.freeSpace/$item.Size)*100)) + "%" }
+    Else 
+    { " It is ok with disk " + $item.DeviceID }
+    }
