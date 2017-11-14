207b750e7bb1eca1801640b14d70816c8cb57a61
kravetsd <kdykrg@gmail.com>
2017-11-14 22:01:43 +0600

    Update mysql_test.php
    Added some code

--- mysql_test.php
+++ mysql_test.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "webapp70533";
+$username = "Dima";
+$password = "KDYkrg@7212";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
